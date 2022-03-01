7dff956c694499273b036f7746e595472cc6ccb8
Mathis Zerbib <mathis.zerbib@gmail.com>
2022-03-01 14:20:06 +0100

    selectors + disbaleSubmit + enable submitBtn

--- starterOnly/js/modal.js
+++ starterOnly/js/modal.js
@@ -8,9 +8,19 @@ function editNav() {
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+let modalbg = document.querySelector(".bground");
+let modalBtn = document.querySelectorAll(".modal-btn");
+let formData = document.querySelectorAll(".formData");
+let closeModal = document.querySelector(".close");
+
+let firstName = document.querySelector('#first')
+let lastName = document.querySelector('#last')
+let email = document.querySelector('#email')
+let birthdate = document.querySelector('#birthdate')
+let gameNumber = document.querySelector("input[type=number]");
+let locationCheckboxes = document.querySelectorAll("input[name='location']");
+let conditionsCheckboxes = document.querySelector("input[name='conditions']");
+let submitBtn = document.querySelector('.btn-submit')
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -18,6 +28,15 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+disableSubmitBtn();
+
+function disableSubmitBtn () {
+  submitBtn.disabled = true;
+}
+
+function enableSubmitBtn () {
+  submitBtn.disabled = false;
 }
 
 
