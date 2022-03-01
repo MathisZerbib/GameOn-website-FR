394ed9e5d6d0b1ca9f5e04e8783756c052fcc854
Mathis Zerbib <mathis.zerbib@gmail.com>
2022-03-01 15:46:33 +0100

    Form check function + first name check lenght + regex + reorganise js

--- starterOnly/js/modal.js
+++ starterOnly/js/modal.js
@@ -1,11 +1,4 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+
 
 // DOM Elements
 let modalbg = document.querySelector(".bground");
@@ -17,18 +10,22 @@ let firstName = document.querySelector('#first')
 let lastName = document.querySelector('#last')
 let email = document.querySelector('#email')
 let birthdate = document.querySelector('#birthdate')
-let gameNumber = document.querySelector("input[type=number]");
+let gameNum = document.querySelector("input[type=number]");
 let locationCheckboxes = document.querySelectorAll("input[name='location']");
 let conditionsCheckboxes = document.querySelector("input[name='conditions']");
 let submitBtn = document.querySelector('.btn-submit')
 
-disableSubmitBtn();
-modalFormLaunch();
-modalFormClose();
-
-
+// Menu burger navigation
+function editNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
 // launch modal event
-function modalFormLaunch () {
+function modalFormLaunch() {
   modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 }
 
@@ -38,34 +35,66 @@ function launchModal() {
 }
 
 // close modal form
-function modalFormClose () {
-  closeModal.onclick = function() {
-  modalbg.style.display = "none";
+function modalFormClose() {
+  closeModal.onclick = function () {
+    modalbg.style.display = "none";
   };
 }
 
 
+// form validation 
+function isFormValid() {
+
+  if (isFirstNameValid()) {
+    enableSubmitBtn();
+    return true;
+  }
+  disableSubmitBtn();
+  return false;
+
+}
 
 
+// first name validation 
 
+function isFirstNameValid() {
+  if (firstName.value.length < 2) {
+    return false;
+  }
 
+  if (firstName.value.length > 15) {
+    return false;
+  }
 
+  if (!/^([^0-9]*)$/.test(firstName.value)) {
+    return false;
+  }
 
-
+  return true;
+}
 
 // disable submit form button
-function disableSubmitBtn () {
+function disableSubmitBtn() {
   submitBtn.disabled = true;
   submitBtn.style.cursor = 'not-allowed';
   submitBtn.style.opacity = '0.5';
 }
 
 // enable submit form button
-function enableSubmitBtn () {
+function enableSubmitBtn() {
   submitBtn.disabled = false;
   submitBtn.style.cursor = 'pointer';
   submitBtn.style.opacity = '1';
 }
+
+
+
+
+
+disableSubmitBtn();
+modalFormLaunch();
+modalFormClose();
+
 
 
 
