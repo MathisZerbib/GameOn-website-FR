177cb30e5105bb482e020c9e3ef13eba0da7bcf3
Mathis Zerbib <mathis.zerbib@gmail.com>
2022-03-01 15:23:18 +0100

    modal form launch wrap + close func

--- starterOnly/js/modal.js
+++ starterOnly/js/modal.js
@@ -22,25 +22,51 @@ let locationCheckboxes = document.querySelectorAll("input[name='location']");
 let conditionsCheckboxes = document.querySelector("input[name='conditions']");
 let submitBtn = document.querySelector('.btn-submit')
 
+disableSubmitBtn();
+modalFormLaunch();
+modalFormClose();
+
+
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+function modalFormLaunch () {
+  modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+}
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
-disableSubmitBtn();
 
+// close modal form
+function modalFormClose () {
+  closeModal.onclick = function() {
+  modalbg.style.display = "none";
+  };
+}
+
+
+
+
+
+
+
+
+
+
+// disable submit form button
 function disableSubmitBtn () {
   submitBtn.disabled = true;
   submitBtn.style.cursor = 'not-allowed';
   submitBtn.style.opacity = '0.5';
 }
 
+// enable submit form button
 function enableSubmitBtn () {
   submitBtn.disabled = false;
   submitBtn.style.cursor = 'pointer';
   submitBtn.style.opacity = '1';
 }
+
+
 
 
